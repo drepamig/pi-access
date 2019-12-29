50f9ac0aec3b6ed99431aeaa2057ac22276b2e92
Ian Moore <you@example.com>
2019-12-29 23:06:11 +0000

    first working proto

--- Read.py
+++ Read.py
@@ -2,12 +2,38 @@
 
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+userAccess = [{"Username": "Test User", "Card_ID":657244891756, "Card_Key": "pimylifeup", "Access": True}]
 
 reader = SimpleMFRC522()
+GPIO.output(7, False)
 
-try:
-        id, text = reader.read()
-        print(id)
-        print(text)
-finally:
-        GPIO.cleanup()
+
+while True:
+    validUser = None
+    try:
+        id, key = reader.read()
+
+        for user in userAccess:
+            if id == user["Card_ID"] and key.strip() == user["Card_Key"]:
+                validUser = user
+                break
+        
+        if validUser:
+            print(f"Hello {validUser['Username']}!")
+            GPIO.output(7, True)
+            sleep(2)
+            GPIO.output(7, False)
+        else:            
+            print(f"I don't know you!: {id}, {key}")
+    except:
+        break
+    
+    if input("Press any key to try again. Press Q to quit").upper() == "Q":
+        break
+
+GPIO.cleanup()
