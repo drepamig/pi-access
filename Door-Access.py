44915b8e71894fb793203ce951619cd790bae069
Ian Moore <you@example.com>
2020-01-19 14:21:34 +0000

    added db caching

--- Door-Access.py
+++ Door-Access.py
@@ -1,8 +1,12 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from time import sleep
+from datetime import datetime
 import MySQLdb
 from typing import Type
+
+# from cachetools import cached, TTLCache
+import threading
 
 # import threading
 # import time
@@ -17,6 +21,8 @@ dbHost = "localhost"
 dbName = "pi-access"
 dbUser = "accessadmin"
 dbPass = "accesspassword"
+dbExpireTime = 60  # minutes
+dbRefreshInterval = 10 # minutes
 
 
 reader = SimpleMFRC522()
@@ -35,14 +41,21 @@ class user_info:  # pylint: disable=too-few-public-methods
     has_access = False
     is_admin = False
 
-    def __init__(self, db_cursor, card_key, card_id):
-        if db_cursor.rowcount != 0:
-            if db_cursor.rowcount != 1:
-                print("WARNING: Multiple matches returned for: {card_id}, {card_key}")
-            db_row = db_cursor.fetchone()
-            self.name: str = db_row["name"]
-            self.has_access: bool = int(db_row["access"]) == 1
-            self.is_admin: bool = int(db_row["admin"]) == 1
+    def __init__(self, results, card_key, card_id):
+        db_row = None
+        # results = db_cursor.fetchall()
+        if (datetime.now() - results.last_refresh).seconds < (dbExpireTime * 60):
+            for row in results.results:
+                if row["card_key"] == card_key and row["card_id"] == str(card_id):
+                    db_row = row
+                    break
+            if db_row is not None:
+                # if db_cursor.rowcount != 1:
+                #     print("WARNING: Multiple matches returned for: {card_id}, {card_key}")
+
+                self.name: str = db_row["name"]
+                self.has_access: bool = int(db_row["access"]) == 1
+                self.is_admin: bool = int(db_row["admin"]) == 1
         self.card_key: str = card_key[:36] if len(card_key) > 36 else card_key
         self.card_id: int = card_id
 
@@ -60,8 +73,39 @@ class door:
         GPIO.output(DOORPIN, False)
 
 
+class _cachedDB:  # pylint: disable=too-few-public-methods
+
+    results = None
+    last_refresh = datetime(2000, 1, 1, 1, 1, 1)
+
+    def __init__(self):
+        threading.Thread(target=self.updateDB).start()
+
+    def updateDB(self):
+        while True:
+            dbConnection = MySQLdb.connect(host=dbHost, user=dbUser, passwd=dbPass, db=dbName)
+            cur = dbConnection.cursor(MySQLdb.cursors.DictCursor)
+            cur.execute(f"SELECT * FROM access_list")
+            dbConnection.close()
+            self.results = cur.fetchall()
+            self.last_refresh = datetime.now()
+            sleep(dbRefreshInterval * 60)
+
+
 def ReaderAccess():
+    # @cached(cache=TTLCache(maxsize=1024, ttl=3600))
+    # def CheckAccess():
+    #     dbConnection = MySQLdb.connect(host=dbHost, user=dbUser, passwd=dbPass, db=dbName)
+    #     cur = dbConnection.cursor(MySQLdb.cursors.DictCursor)
+    #     cur.execute(f"SELECT * FROM access_list")
+    #     dbConnection.close()
+    #     return cur.fetchall()
+
+    cachedDB = _cachedDB()
+
     def log_access(u: Type[user_info]):
+        dbConnection = MySQLdb.connect(host=dbHost, user=dbUser, passwd=dbPass, db=dbName)
+        cur = dbConnection.cursor(MySQLdb.cursors.DictCursor)
         cur.execute(
             f"INSERT INTO access_log SET card_key_presented = '{u.card_key}', card_key_presented_datetime = NOW(), card_id_presented = '{u.card_id}', access_granted = '{1 if u.has_access else 0}', name_presented = '{u.name}'"
         )
@@ -69,18 +113,12 @@ def ReaderAccess():
         pass
 
     door.close()
-    
 
     while True:
         print("Waiting for key card...")
         card_id, card_key = reader.read()
         card_key = card_key.strip()
-        dbConnection = MySQLdb.connect(host=dbHost, user=dbUser, passwd=dbPass, db=dbName)
-        cur = dbConnection.cursor(MySQLdb.cursors.DictCursor)
-        cur.execute(f"SELECT * FROM access_list WHERE card_key = '{card_key}' and card_id = '{card_id}'")
-        u = user_info(cur, card_key, card_id)
-        log_access(u)
-        dbConnection.close()
+        u = user_info(cachedDB, card_key, card_id)
 
         if u.has_access:
             print(f"\tACCESS GRANTED for {u.name}")
@@ -88,6 +126,8 @@ def ReaderAccess():
         else:
             print("\tACCESS DENIED")
             sleep(RELAY_TIMEOUT)
+
+        threading.Thread(target=log_access, args=[u]).start()
 
         # dbConnection.commit()
 
